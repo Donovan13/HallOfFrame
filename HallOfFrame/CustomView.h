077b837332840ed99c9e5a5e1c333acb90ca3211
Kyle <Ky@Kyles-MacBook-Pro.local>
2016-03-23 15:56:25 -0500

    push 5

---
+++ HallOfFrame/CustomView.h
@@ -0,0 +1,15 @@
+//
+//  CustomView.h
+//  HallOfFrame
+//
+//  Created by Kyle on 3/23/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+
+
+@end
---
+++ HallOfFrame/CustomView.m
@@ -0,0 +1,28 @@
+//
+//  CustomView.m
+//  HallOfFrame
+//
+//  Created by Kyle on 3/23/16.
+//  Copyright © 2016 Kyle. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+- (IBAction)redButtonTapped:(UIButton *)sender {
+}
+- (IBAction)blueButtonTapped:(UIButton *)sender {
+}
+- (IBAction)greenButtonTapped:(UIButton *)sender {
+}
+
+
+@end
